4b61674dfb188517cbfda8234ec91285cf280135
Dattatreya Sengupta <dattatreyasengupta@Dattatreyas-MacBook-Pro.local>
2026-09-16 19:08:01 +0530

    added bearer token in api call

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,22 @@
+import { getAccessToken } from "../services/auth.service";
+
 const BASE_URL = "http://localhost:3000/api/v1"
 
 export async function api<T>(endpoint: string, options?: RequestInit): Promise<T>{
-    const response = await fetch(`${BASE_URL}${endpoint}`, options);
+    
+    const token = getAccessToken();
+
+    const headers = new Headers(options.headers);
+
+    if(token){
+        headers.set("Authorization", `Bearer ${token}`);
+    }
+    
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        ...options,
+        headers,
+        credentials: "include"
+    });
 
     if (!response.ok){
         throw new Error(`Request failed: ${response.status}`);
